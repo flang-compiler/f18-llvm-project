beb43ff9c88369dd7f39f55d35e72d41d23a6dab
Valentin Clement <clementval@gmail.com>
2021-01-29 10:41:11 -0500

    [flang] Add -split-input-file -verify-diagnostics options to TCO

--- flang/tools/tco/tco.cpp
+++ flang/tools/tco/tco.cpp
@@ -23,6 +23,7 @@
 #include "mlir/Parser.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
+#include "mlir/Support/ToolUtilities.h"
 #include "mlir/Transforms/Passes.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/ErrorOr.h"
@@ -52,50 +53,51 @@ static cl::opt<std::string> targetTriple("target",
                                          cl::desc("specify a target triple"),
                                          cl::init("native"));
 
+static cl::opt<bool>
+    splitInputFile("split-input-file",
+                   cl::desc("Split the input file into pieces and process each "
+                            "chunk independently"),
+                   cl::init(false));
+
+static cl::opt<bool>
+    verifyDiagnostics("verify-diagnostics",
+                      cl::desc("Check that emitted diagnostics match "
+                               "expected-* lines on the corresponding line"),
+                      cl::init(false));
+
 static void printModuleBody(mlir::ModuleOp mod, raw_ostream &output) {
   for (auto &op : mod.getBody()->without_terminator())
     output << op << '\n';
 }
 
-// compile a .fir file
 static mlir::LogicalResult
-compileFIR(const mlir::PassPipelineCLParser &passPipeline) {
-  // check that there is a file to load
-  ErrorOr<std::unique_ptr<MemoryBuffer>> fileOrErr =
-      MemoryBuffer::getFileOrSTDIN(inputFilename);
+performActions(raw_ostream &os, SourceMgr &sourceMgr,
+               const mlir::PassPipelineCLParser &passPipeline, bool emitFir,
+               mlir::MLIRContext *context) {
+  // Disable multi-threading when parsing the input file. This removes the
+  // unnecessary/costly context synchronization when parsing.
+  bool wasThreadingEnabled = context->isMultithreadingEnabled();
+  context->disableMultithreading();
 
-  if (std::error_code EC = fileOrErr.getError()) {
-    errs() << "Could not open file: " << EC.message() << '\n';
+  // Parse the input file and reset the context threading state.
+  mlir::OwningModuleRef module(parseSourceFile(sourceMgr, context));
+  context->enableMultithreading(wasThreadingEnabled);
+  if (!module)
     return mlir::failure();
-  }
 
-  // load the file into a module
-  SourceMgr sourceMgr;
-  sourceMgr.AddNewSourceBuffer(std::move(*fileOrErr), SMLoc());
-  mlir::MLIRContext context;
-  fir::registerAndLoadDialects(context);
-  auto owningRef = mlir::parseSourceFile(sourceMgr, &context);
-
-  if (!owningRef) {
-    errs() << "Error can't load file " << inputFilename << '\n';
-    return mlir::failure();
-  }
-  if (mlir::failed(owningRef->verify())) {
+  if (mlir::failed(module->verify())) {
     errs() << "Error verifying FIR module\n";
     return mlir::failure();
   }
 
-  std::error_code ec;
-  ToolOutputFile out(outputFilename, ec, sys::fs::OF_None);
-
   // run passes
   llvm::Triple triple(fir::determineTargetTriple(targetTriple));
   fir::NameUniquer uniquer;
-  fir::KindMapping kindMap{&context};
-  fir::setTargetTriple(*owningRef, triple);
-  fir::setNameUniquer(*owningRef, uniquer);
-  fir::setKindMapping(*owningRef, kindMap);
-  mlir::PassManager pm(&context, mlir::OpPassManager::Nesting::Implicit);
+  fir::KindMapping kindMap{context};
+  fir::setTargetTriple(*module, triple);
+  fir::setNameUniquer(*module, uniquer);
+  fir::setKindMapping(*module, kindMap);
+  mlir::PassManager pm(context, mlir::OpPassManager::Nesting::Implicit);
   pm.enableVerifier(/*verifyPasses=*/true);
   mlir::applyPassManagerCLOptions(pm);
   if (emitFir) {
@@ -103,7 +105,7 @@ compileFIR(const mlir::PassPipelineCLParser &passPipeline) {
     // -emit-fir intentionally disables all the passes
   } else if (passPipeline.hasAnyOccurrences()) {
     passPipeline.addToPipeline(pm, [&](const Twine &msg) {
-      mlir::emitError(mlir::UnknownLoc::get(&context)) << msg;
+      mlir::emitError(mlir::UnknownLoc::get(context)) << msg;
       return mlir::failure();
     });
   } else {
@@ -126,22 +128,86 @@ compileFIR(const mlir::PassPipelineCLParser &passPipeline) {
     pm.addPass(fir::createFirCodeGenRewritePass());
     pm.addPass(fir::createFirTargetRewritePass());
     pm.addPass(fir::createFIRToLLVMPass(uniquer));
-    pm.addPass(fir::createLLVMDialectToLLVMPass(out.os()));
+    pm.addPass(fir::createLLVMDialectToLLVMPass(os));
   }
 
   // run the pass manager
-  if (mlir::succeeded(pm.run(*owningRef))) {
+  if (mlir::succeeded(pm.run(*module))) {
     // passes ran successfully, so keep the output
     if (emitFir || passPipeline.hasAnyOccurrences())
-      printModuleBody(*owningRef, out.os());
-    out.keep();
+      printModuleBody(*module, os);
     return mlir::success();
   }
 
-  // pass manager failed
-  printModuleBody(*owningRef, errs());
-  errs() << "\n\nFAILED: " << inputFilename << '\n';
   return mlir::failure();
+}
+
+static mlir::LogicalResult
+processBuffer(raw_ostream &os, std::unique_ptr<MemoryBuffer> ownedBuffer,
+              const mlir::PassPipelineCLParser &passPipeline, bool emitFir,
+              bool verifyDiagnostics) {
+  // Tell sourceMgr about this buffer, which is what the parser will pick up.
+  SourceMgr sourceMgr;
+  sourceMgr.AddNewSourceBuffer(std::move(ownedBuffer), SMLoc());
+
+  mlir::MLIRContext context;
+  fir::registerAndLoadDialects(context);
+  context.printOpOnDiagnostic(!verifyDiagnostics);
+
+  if (!verifyDiagnostics) {
+    mlir::SourceMgrDiagnosticHandler sourceMgrHandler(sourceMgr, &context);
+    return performActions(os, sourceMgr, passPipeline, emitFir, &context);
+  }
+
+  mlir::SourceMgrDiagnosticVerifierHandler sourceMgrHandler(sourceMgr,
+                                                            &context);
+
+  // Do any processing requested by command line flags.  We don't care whether
+  // these actions succeed or fail, we only care what diagnostics they produce
+  // and whether they match our expectations.
+  performActions(os, sourceMgr, passPipeline, emitFir, &context);
+
+  // Verify the diagnostic handler to make sure that each of the diagnostics
+  // matched.
+  return sourceMgrHandler.verify();
+}
+
+// compile a .fir file
+static mlir::LogicalResult
+compileFIR(const mlir::PassPipelineCLParser &passPipeline) {
+  // check that there is a file to load
+  ErrorOr<std::unique_ptr<MemoryBuffer>> fileOrErr =
+      MemoryBuffer::getFileOrSTDIN(inputFilename);
+
+  if (std::error_code EC = fileOrErr.getError()) {
+    errs() << "Could not open file: " << EC.message() << '\n';
+    return mlir::failure();
+  }
+
+  mlir::MLIRContext context;
+  fir::registerAndLoadDialects(context);
+
+  std::error_code ec;
+  ToolOutputFile out(outputFilename, ec, sys::fs::OF_None);
+
+  if (splitInputFile)
+    return mlir::splitAndProcessBuffer(
+        std::move(*fileOrErr),
+        [&](std::unique_ptr<MemoryBuffer> chunkBuffer, raw_ostream &os) {
+          return processBuffer(out.os(), std::move(chunkBuffer), passPipeline,
+                               emitFir, verifyDiagnostics);
+        },
+        out.os());
+
+  if (failed(processBuffer(out.os(), std::move(*fileOrErr), passPipeline,
+                           emitFir, verifyDiagnostics))) {
+    errs() << "\n\nFAILED: " << inputFilename << '\n';
+    return mlir::failure();
+  }
+
+  // Keep the output file if the invocation was successful.
+  out.keep();
+  return mlir::success();
 }
 
 int main(int argc, char **argv) {
