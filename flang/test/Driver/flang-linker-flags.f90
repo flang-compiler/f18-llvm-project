ce603ad79d35e15e58044b27cb0adb94f317c7c2
Andrzej Warzynski <andrzej.warzynski@arm.com>
2021-11-09 17:16:58 +0000

    [flang] Add Fortran runtime libs to the linker invocation
    This patch adds the Fortran runtime libraries to the linker
    invocation generated by `clangDriver`.
    
    With this change, the following invocation will generate an executable:
    ```
    flang-new hello.f90
    ```

---
+++ flang/test/Driver/flang-linker-flags.f90
@@ -0,0 +1,17 @@
+! Verify that the Fortran runtime libraries are present in the linker
+! invocation. These libraries are added on top of other standard runtime
+! libraries that the Clang driver will include.
+
+! NOTE: The additional linker flags tested here are currently specified in
+! clang/lib/Driver/Toolchains/Gnu.cpp. This makes the current implementation GNU
+! (Linux) specific. The following line will make sure that this test is skipped
+! on Windows. Ideally we should find a more robust way of testing this.
+! REQUIRES: shell
+
+! RUN: %flang -### %S/Inputs/hello.f90 2>&1 | FileCheck %s
+
+! CHECK-LABEL: "/usr/bin/ld"
+! CHECK: -lFortran_main
+! CHECK: -lFortranRuntime
+! CHECK: -lFortranDecimal
+! CHECK: -lm
